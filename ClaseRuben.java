db6b8a0f5b9ac8ac70a7f1b295c4dd94c2c1db57
Rubenberdejo <berdejo.esrub22@zaragoza.salesianos.edu>
2023-05-09 16:36:19 +0200

    Hora: 16:35 -- Ruben Berdejo Escanilla

---
+++ ClaseRuben.java
@@ -0,0 +1,17 @@
+public class ClaseRuben
+{
+
+    String nombre;
+
+    public ClaseRuben(String as_nombre)
+    {
+        nombre = as_nombre;   
+    }
+
+    // clase donde se escribe el nombre
+    public void escribirnombre()
+    {
+        System.out.println("Mi nombre es: " + nombre);
+    }
+
+}
