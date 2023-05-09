6a754ba928324164e46c1c7c780ae3091645c927
valeriin18 <valero.ladan18@zaragoza.salesianos.edu>
2023-05-09 16:41:53 +0200

    Valero a las 16:40 el 9 de mayo de 2023
    Añado el archivo ClaseValero.java para que se muestre mi linea de codigo al ejecutar el archivo Examen.java

---
+++ ClaseValero.java
@@ -0,0 +1,11 @@
+
+public class ClaseValero {
+    String nombre;
+    public ClaseValero(String as_nombre){
+        nombre = as_nombre;   
+    }
+    // clase donde se escribe el nombre
+    public void escribirnombre(){
+        System.out.println("Mi nombre es: " + nombre);
+    }
+}
